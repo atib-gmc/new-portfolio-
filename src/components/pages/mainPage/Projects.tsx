bd8ae0376404fd7463995bbef8dbba81bafa2e84
= <=>
2024-02-17 06:19:03 +0700

    add features section to projects

--- src/components/pages/mainPage/Projects.tsx
+++ src/components/pages/mainPage/Projects.tsx
@@ -12,6 +12,13 @@ const projects: Project[] = [
       "This project aims to develop a modern and interactive social media application leveraging the power of React, Firebase, and TypeScript. The application focuses on providing users with a seamless and engaging platform for social networking while incorporating robust functionalities and a user-friendly interface.",
     gitHub: "https://github.com/adios35/socialMediaAppFireBase",
     url: "https://social-media-app-fire-base.vercel.app",
+    features: [
+      "responsive mobile first",
+      "authentication",
+      "image upload",
+      "comment and like post",
+      "follow unfollow",
+    ],
     imgs: [
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1704549580/porto/flvxinbevgxnp0jypvti.png",
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1706929777/kyflfvdcgsspdfwjbs5o.png",
@@ -25,25 +32,39 @@ const projects: Project[] = [
       "This eCommerce project with user-friendly interface powered by React Typescript and Firebase. It encompasses essential functionalities like cart management, user registration, and login features. With real-time updates from Firebase, this platform ensures interactive UI, providing a seamless shopping experience for all users.",
     url: "https://e-commerce-firebase.netlify.app/",
     gitHub: "",
+    features: [
+      "fully functional shopping cart ",
+      "responsive",
+      "dark mode",
+      "serach product",
+      "complex routing",
+    ],
     imgs: [
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1704549579/porto/v1tdfidm24k8zw9v6agp.png",
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1704549579/porto/q8ty9ymgjp5vnikyt0d7.png",
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1704549578/porto/aqexqcsajgo2hb675vr7.png",
     ],
-  }, {
+  },
+  {
     name: "fullstack-blog-app(mern + jwt)",
     stacks: ["React", "Typesript", "Tailwindcss", "Jwt", "Express", "NodeJs"],
     description:
       "this is my first fullstack app, and this is one of my project based learning while learning backend with express as backend database with mongo db , the hardest challange i faced is creating authentication with jwt",
     url: "https://incredible-axolotl-710e61.netlify.app/",
     gitHub: "",
+    features: [
+      "authentication with jwt",
+      "Crud",
+      "responsive mobile first",
+      "comment",
+    ],
     imgs: [
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1705311715/porto/swsym7mk8gskhhmdnqfj.png",
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1705311716/porto/ccjjde1fyglzqy9nl4qx.png",
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1704549833/v5r5pc5g8jikt7rbpstj.png",
       "https://res.cloudinary.com/dhvra0tqf/image/upload/v1705311715/porto/kjiat5fbcior6e93bxlm.png",
     ],
-  }
+  },
 ];
 
 const Projects = () => {
@@ -53,7 +74,6 @@ const Projects = () => {
       <hr className="divider my-8 border-t-2 border-dashed mx-auto  border-main-dark" />
       <section id="projects">
         <Header className="text-xl pt-[4px] block py-2">Projects</Header>
-
       </section>
       {projects.map((project, i) => (
         <ProjectCard key={i} project={project} id={i} />
--- src/components/pages/mainPage/index.tsx
+++ src/components/pages/mainPage/index.tsx
@@ -11,16 +11,28 @@ const Main = () => {
 
   return (
     <div className=" mx-auto   border-t-2 border-main-dark pt-8 border-dashed  w-full space-y-5">
-      <Header id="about" className="text-xl pt-[4px] block py-2">Greetings</Header>
+      <Header id="about" className="text-xl pt-[4px] block py-2">
+        Greetings
+      </Header>
 
       <section className="skills text-justify justify-center  flex flex-wrap gap-4">
         <p className="   ">
           My name is Atib i'am self taught web developer from Lebak Banten,
-          welcome to my portfolio
+          welcome to my portfolio.
         </p>
         {showMore && (
           <p>
-            i am an intpt who love tech and cup of coffe
+            I am a fullstack web developer who focus on frontend and also
+            understand some backend, i start my web dev journey about two year
+            ago and start more focused in early this year, despite bing self
+            taught, i have manage teach myself to become full stack web
+            developer, i can create beautifull and well animated responsive
+            frontend app and also create backend api with expressJs, i mainly
+            using react and nextjs and sometimes using vanila for my project and
+            i love using framer-motion for the animation, and i using expressJs
+            for my backend mongoDb as database ( m-e-r-n ). i spend atleast two
+            hour a day after work for coding and sometimes more because i love
+            learning and it kinda become my hobbies.
           </p>
         )}
         <CustomButton onClick={() => setShowMore((pre) => !pre)}>
@@ -33,9 +45,41 @@ const Main = () => {
       </section>
       <hr className="divider my-8 border-t-2 border-dashed mx-auto  border-main-dark" />
       <section className="skills">
-        <Header id="skills" className="text-xl pt-[4px] block py-2">My Skills</Header>
-        <Collapse title="frontend" skills={["Html", "Css", "Javascript", "Sass", "Typescript", "React", "Tailwindcss", "NextJs"]} />
-        <Collapse title="Backend" skills={["NodeJs", "ExpressJs", "Mysql", "MongoDb", "PrismaJs", "Authentication", "Cookie", "Session", "php( currently learning )", "Laravel(currently learning)"]} />
+        <Header id="skills" className="text-xl pt-[4px] block py-2">
+          My Skills
+        </Header>
+        <Collapse
+          title="frontend"
+          skills={[
+            "Html",
+            "Css",
+            "Javascript",
+            "Sass",
+            "Typescript",
+            "React",
+            "Tailwindcss",
+            "framer-motion",
+            "NextJs",
+            "Redux",
+            "Zustand",
+            "React Native ( currently study )",
+          ]}
+        />
+        <Collapse
+          title="Backend"
+          skills={[
+            "NodeJs",
+            "ExpressJs",
+            "Mysql",
+            "MongoDb",
+            "PrismaJs",
+            "Authentication",
+            "Cookie",
+            "Session",
+            "php( currently study )",
+            "Laravel(currently study)",
+          ]}
+        />
       </section>
       {/* projects */}
       <Projects />
